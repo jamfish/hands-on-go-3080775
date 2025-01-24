f12fe10ee33030b3f6f58b42ee55e931286a7807
Jam Leomi <3192230+jamfish@users.noreply.github.com>
2025-01-24 06:32:33 +0000

    Redoing the exercises since it's been a while.

--- packages/basics/main.go
+++ packages/basics/main.go
@@ -1,5 +1,16 @@
 // packages/basics/main.go
 package main
 
+//import  the fmt package from the standard library
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
+	//user the fmt package to print the string "Hello Gopher"
+	fmt.Println("Hello Gopher!")
+
+	// use the time package to print the current workday
+	fmt.Printf("Today is %s", time.Now().Weekday())
 }
